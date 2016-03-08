343dddd242ca0564cea1e8f62ec3a970955e133c
Allen Kramer <kramer.alleng@gmail.com>
2016-03-08 17:56:55 -0500

    added wikis CRUD

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,2 +1,60 @@
 class WikisController < ApplicationController
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.create(wiki_params)
+
+    if @wiki.save
+      redirect_to @wiki, notice: "Wiki saved successfully."
+    else
+      flash.new[:alert] = "Error creating wiki.  Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.assign_attributes(wiki_params)
+
+    if @wiki.save
+      flash[:notice] = "Wiki has been updated."
+      redirect_to @wiki
+    else
+      flash[:alert] = "Error saving Wiki.  Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+
+
+  private
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :public)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
+  resources :wikis
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
@@ -9,17 +11,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
