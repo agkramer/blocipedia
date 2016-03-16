550a78743323030fc15d5499aecc52aee3d93093
Allen Kramer <kramer.alleng@gmail.com>
2016-03-16 10:03:02 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(
+  email: "kramer.alleng@gmail.com",
+  password: "helloworld",
+  role: "admin"
+)
+
+User.create!(
+  email:    "standard@blocipedia.com",
+  password: "helloworld",
+  role:     "standard"
+)
+
+User.create!(
+email:    "premium@blocipedia.com",
+password: "helloworld",
+role:     "premium"
+)
+
+5.times do
+  User.create!(
+    email:    Faker::Internet.email,
+    password: "helloworld",
+    role:     "standard"
+  )
+end
+
+5.times do
+  User.create!(
+    email:    Faker::Internet.email,
+    password: "helloworld",
+    role:     "premium"
+  )
+end
+users = User.all
+
+30.times do
+  Wiki.create!(
+    title:    Faker::Hipster.word,
+    body:     Faker::Hipster.paragraph,
+    private:  Faker::Boolean.boolean,
+    user:     users.sample
+  )
+end
+wikis = Wiki.all
