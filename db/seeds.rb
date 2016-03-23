c6c0ae495434c66f3880aa64f9470f2b057083b1
Allen Kramer <kramer.alleng@gmail.com>
2016-03-22 22:34:54 -0400

    added back seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,44 @@
-# User.create!(
-#   email: "kramer.alleng@gmail.com",
-#   password: "helloworld",
-#   role: "admin"
-# )
-#
-# User.create!(
-#   email:    "standard@blocipedia.com",
-#   password: "helloworld",
-#   role:     "standard"
-# )
-#
-# User.create!(
-# email:    "premium@blocipedia.com",
-# password: "helloworld",
-# role:     "premium"
-# )
-#
-# 5.times do
-#   User.create!(
-#     email:    Faker::Internet.email,
-#     password: "helloworld",
-#     role:     "standard"
-#   )
-# end
-#
-# 5.times do
-#   User.create!(
-#     email:    Faker::Internet.email,
-#     password: "helloworld",
-#     role:     "premium"
-#   )
-# end
-# users = User.all
-#
-# 30.times do
-#   Wiki.create!(
-#     title:    Faker::Hipster.word,
-#     body:     Faker::Hipster.paragraph,
-#     private:  Faker::Boolean.boolean,
-#     user:     users.sample
-#   )
-# end
-# wikis = Wiki.all
+User.create!(
+  email: "kramer.alleng@gmail.com",
+  password: "helloworld",
+  role: "admin"
+)
+
+User.create!(
+  email:    "standard@blocipedia.com",
+  password: "helloworld",
+  role:     "standard"
+)
+
+User.create!(
+email:    "premium@blocipedia.com",
+password: "helloworld",
+role:     "premium"
+)
+
+5.times do
+  User.create!(
+    email:    Faker::Internet.email,
+    password: "helloworld",
+    role:     "standard"
+  )
+end
+
+5.times do
+  User.create!(
+    email:    Faker::Internet.email,
+    password: "helloworld",
+    role:     "premium"
+  )
+end
+users = User.all
+
+30.times do
+  Wiki.create!(
+    title:    Faker::Hipster.word,
+    body:     Faker::Hipster.paragraph,
+    private:  Faker::Boolean.boolean,
+    user:     users.sample
+  )
+end
+wikis = Wiki.all
